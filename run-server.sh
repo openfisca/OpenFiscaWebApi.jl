2235023e5138f56df49f7018345a125f6f988656
Christophe Benz <christophe.benz@data.gouv.fr>
2015-02-16 15:58:53 +0100

    Add -r flag

--- run-server.sh
+++ run-server.sh
@@ -4,7 +4,7 @@ while true; do
   echo "=== Loading server…"
   julia start_app.jl &
   PID=$!
-  inotifywait -e modify -e close_write -e create -e delete src
+  inotifywait -r -e modify -e close_write -e create -e delete src
   echo "=== File changed, killing server…"
   kill -SIGINT $PID
   echo -e "\n\n\n\n\n\n"
