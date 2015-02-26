05cd8215bc9de35a1a088558294874c8fbca684a
Christophe Benz <christophe.benz@data.gouv.fr>
2015-02-26 19:27:29 +0100

    Enhance calculate controller

--- src/controllers/calculate.jl
+++ src/controllers/calculate.jl
@@ -21,22 +21,75 @@
 
 
 using Biryani
+using Biryani.JsonConverters
 using Dates
 import JSON
 
-using OpenFiscaCore: calculate, Simulation, to_scenario, YearPeriod
+using OpenFiscaCore: calculate, get_array, get_variable, Simulation, to_scenario, YearPeriod
 using OpenFiscaFrance: tax_benefit_system
 
 
+function build_test_case(scenario, simulation, variables)
+    new_test_case = deepcopy(scenario.test_case)
+    for variable_name in variables
+        variable = get_variable(simulation, variable_name)
+        variable_value_json = [
+          string(period) => [
+            variable.definition.cell_to_value_at_period_json(cell)
+            for cell in array
+          ]
+          for (period, array) in variable.array_by_period
+        ]
+        entity_members = new_test_case[variable.definition.entity_definition.name_plural]
+        for (entity_member_index, entity_member) in enumerate(entity_members)
+            entity_member[variable.definition.name] = [
+                period => array_json[entity_member_index]
+                for (period, array_json) in variable_value_json
+            ]
+        end
+    end
+    return new_test_case
+end
+
+
 function handle_calculate(req, res)
-  inputs = req.http_req.data
-  params = JSON.parse(inputs) # TODO use biryani converter
-  params_to_data = struct(
+  input_to_params = pipe(
+    input_to_json,
+    test_isa(Dict),
+    require,
+  )
+  params, error = Convertible(req.http_req.data) |> input_to_params |> to_value_error
+  if error !== nothing
+    return bad_request(res, errors = [error])
+  end
+
+  const default_scenario = [
+    "period" => YearPeriod(2013),
+    "test_case" => [
+      "individus" => [(String => Any)[]],
+    ],
+  ]
+  const params_to_data = struct(
     [
+      "scenarios" => pipe(
+        default([default_scenario]),
+        uniform_sequence(
+          require, # Real conversion is done after.
+        ),
+        test(scenarios -> length(scenarios) >= 1, error = N_("At least one scenario is required")),
+        test(scenarios -> length(scenarios) <= 100, error = N_("There can't be more than 100 scenarios")),
+        require,
+      ),
+      "validate" => pipe(
+        test_isa(Union(Bool, Int)),
+        to_bool,
+        default(false),
+      ),
       "variables" => pipe(
         test_isa(Array),
         uniform_sequence(
           test_isa(String),
+          test_in(tax_benefit_system.variable_definition_by_name),
           require,
         ),
         empty_to_nothing,
@@ -45,19 +98,31 @@ function handle_calculate(req, res)
     ],
   )
   data, errors = Convertible(params) |> params_to_data |> to_value_error
+  if errors === nothing
+    scenarios, errors = Convertible(data["scenarios"]) |> uniform_sequence(
+      to_scenario(tax_benefit_system, repair = data["validate"]),
+      require,
+    ) |> to_value_error
+  end
   if errors !== nothing
     return bad_request(res, errors = errors)
   end
 
-  scenario = Convertible([
-    "test_case" => [
-      "individus" => [(String => Any)[]],
-    ],
-    "year" => 2013,
-  ]) |> to_scenario(tax_benefit_system, repair = true) |> to_value
-  simulation = Simulation(scenario, debug = true)
-  value = calculate(simulation, "irpp")
-  response_data = [
+  simulations = map(scenarios) do scenario
+    simulation = Simulation(scenario, debug = true, trace = false)
+    for variable in data["variables"]
+      calculate(simulation, variable)
+    end
+    return simulation
+  end
+
+  value = [
+    build_test_case(scenario, simulation, data["variables"])
+    for (scenario, simulation) in zip(scenarios, simulations)
+  ]
+
+  const response_data = [
+    "params" => params,
     "value" => value,
   ]
   return respond_json(res, data = response_data)
